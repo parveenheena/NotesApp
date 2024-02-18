59a37e2e4e9b98970dd098216d32efac5321abb4
heena25 <parveenheena196@gmail.com>
2024-02-18 15:08:28 +0530

    added swagger api

--- Routes/notesRoutes.js
+++ Routes/notesRoutes.js
@@ -64,4 +64,132 @@ notesRouter.delete("/:noteid",auth,async(req,res)=>{
     }
 })
 
+
+//swagger api
+
+ /**
+ * @swagger
+ * /notes:
+ *   post:
+ *     summary: Create a new note
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Note"
+ *     responses:
+ *       '200':
+ *         description: New note created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Note"
+ *
+ *   get:
+ *     summary: Get all notes
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       '200':
+ *         description: Retrieved notes successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/Note"
+ *
+ * /notes/{id}:
+ *   get:
+ *     summary: Get a note by ID
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the note to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Retrieved note successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Note"
+ *
+ *   patch:
+ *     summary: Update a note by ID
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: noteid
+ *         in: path
+ *         required: true
+ *         description: ID of the note to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Note"
+ *     responses:
+ *       '200':
+ *         description: Note updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *
+ *   delete:
+ *     summary: Delete a note by ID
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: noteid
+ *         in: path
+ *         required: true
+ *         description: ID of the note to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Note deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *
+ * components:
+ *   schemas:
+ *     Note:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *         content:
+ *           type: string
+ *         userId:
+ *           type: string
+ */
+
+
+
 module.exports = {notesRouter}
--- index.js
+++ index.js
@@ -5,12 +5,33 @@ const { notesRouter } = require("./Routes/notesRoutes")
 require("dotenv").config()
 const app = express()
 const cors = require("cors")
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express')
 app.use(express.json())
 app.use(cors())
 app.use("/notes",notesRouter)
 app.use("/users",userRouter)
 
+
+
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Notes App',
+        version: '1.3.0',
+      },
+      servers:[
+          {
+              url : "http://localhost:8080/"
+          }
+      ]
+    },
+    apis: ['./Routes/*.js'], 
+  };
+  
+  const openapiSpecification = swaggerJsdoc(options);
+  app.use("/apidocs",swaggerUi.serve,swaggerUi.setup(openapiSpecification))
 
 
 app.listen(process.env.port,async()=>{
